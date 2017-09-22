31b0e0f96973e1729ce05652900feeba6e7de3b5
liubin <752791962@qq.com>
2017-09-22 16:36:09 +0800

    233

--- lib/image.func.php
+++ lib/image.func.php
@@ -1,18 +1,41 @@
-<?php
+<?php session_start();
 /**
  * Created by PhpStorm.
  * User: Administrator
  * Date: 2017/9/14
  * Time: 9:32
  */
-//通过GD库做验证码
-//创建画布
-$width = 80;
-$height = 20;
-$image = imagecreatetruecolor($width, $height);
-$white = imagecolorallocate($image,255,255,255); //为一幅图像分配颜色
-$black = imagecolorallocate($image, 0,0,0);
-//用填充矩形填充画布
-
-
-
+require_once 'string.func.php';
+function verifyImage($type = 1, $length = 4, $pixel = 0, $sess_name = 'verify'){
+    //通过GD库做验证码
+    //创建画布
+    $width = 80;
+    $height = 28;
+    $image = imagecreatetruecolor($width, $height);
+    $white = imagecolorallocate($image,255,255,255); //为一幅图像分配颜色
+    $black = imagecolorallocate($image, 0,0,0);
+    //用填充矩形填充画布
+    imagefilledrectangle($image, 1,1, $width-2,$height-2, $white);
+    $chars = buidRandomString($type, $length);
+    $_SESSION[$sess_name] = $chars;
+    $fontfiles = array("msyh.ttf","msyhbd.ttf","simfang.ttf","simhei.ttf","simkai.ttf","simsun.ttc");
+    for($i=0; $i<$length; $i++) {
+        $size = mt_rand(14, 18);
+        $angle = mt_rand(-15, 15);
+        $x = 5 + $i*$size;
+        $y = mt_rand(20,26);
+        $color = imagecolorallocate($image, mt_rand(50, 90), mt_rand(80, 200), mt_rand(90, 180));
+        $fontfile = "../fonts/".$fontfiles[mt_rand(0, count($fontfiles)-1)];
+        $text = substr($chars, $i, 1);
+        imagettftext($image, $size, $angle, $x, $y, $color, $fontfile, $text);
+    }
+    if ($pixel){
+        for ($i=0; $i<$pixel; $i++){
+            imagesetpixel($image, mt_rand(1, $width-1), mt_rand(1, $height-1), imagecolorallocate($image, mt_rand(50, 90), mt_rand(80, 200), mt_rand(90, 180)));
+        }
+    }
+        header("content-type:image/gif;charset='utf-8'");
+        imagegif($image);
+        imagedestroy($image);
+}   
+?>
--- lib/string.func.php
+++ lib/string.func.php
@@ -5,3 +5,17 @@
  * Date: 2017/9/14
  * Time: 9:33
  */
+function buidRandomString($type = 1, $length = 4){
+    if ($type == 1) {
+        $chars = join("", range(0, 9));
+    } else if($type == 2) {
+        $chars = join("", array_merge(range("a", "z"), range("A", "Z")));
+    } else if($type == 3) {
+        $chars = join("", array_merge(range("a", "z"), range("A", "Z"), range(0, 9)));
+    }
+    if ($length>strlen($chars)) {
+        exit("字符串长度不够");
+    }
+    $chars = str_shuffle($chars);//打乱数组
+    return substr($chars, 0, $length);
+}
